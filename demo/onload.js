9ec9e62341f5afb2e44a089a6c995c78db8c65c7
Chris Mueller <chris@thumbtack.com>
2012-02-27 16:18:39 -0800

    Style the demo page

--- demo/onload.js
+++ demo/onload.js
@@ -1,7 +1,17 @@
 $(function() {
+    // override default result colors for charts
+    Abba.RESULT_COLORS = {
+        neutral: '#D8D8D8',
+        lose: '#B42647',
+        win: '#26B43C'
+    };
+
     var presenter = new Abba.Presenter(Abba.Abba);
     presenter.bind(
         new Abba.InputsView($('#inputs'), document.getElementById('hidden-iframe')),
         $('#results')
     );
+
+    // trigger animations
+    $("h1").addClass("animate");
 });
